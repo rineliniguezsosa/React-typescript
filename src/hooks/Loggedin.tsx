d6f96f19935fcad8e1dc2e7a1decc75322aaf5a8
rinel <rineliniguezsosa@gmail.com>
2023-06-06 17:02:55 -0600

    feat(Loggedin): creando el elemento span con el titulo: El usuario esta autenticado/logout

--- src/hooks/Loggedin.tsx
+++ src/hooks/Loggedin.tsx
@@ -5,6 +5,7 @@ export const Loggedin = () => {
     <div>
         <button>Login</button>
         <button>Logout</button>
+        <span>El usuario esta autenticado/logout</span>
     </div>
   )
 }
