34479fa63db5565b1cb0d41becb06f216be084d9
rinel <rineliniguezsosa@gmail.com>
2023-06-07 14:33:42 -0600

    feat(ThemeContext): pasando el objeto theme a la creación del contexto para propagarlo hacia mis componentes

--- src/hooks/Context/ThemeContext.tsx
+++ src/hooks/Context/ThemeContext.tsx
@@ -1,4 +1,4 @@
 import { createContext } from 'react'
 import { theme } from './Theme'
 
-export const ThemeContext = createContext()
+export const ThemeContext = createContext(theme)//que voy a propagar hacia mis componentes
