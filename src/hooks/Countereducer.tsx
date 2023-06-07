8aa17b3c9264bb03de8345d2f26593c91cef5d65
rinel <rineliniguezsosa@gmail.com>
2023-06-06 18:49:10 -0600

    feat(Countereducer): definiendo el bloque switch case donde evaluaremos el action.payload

--- src/hooks/Countereducer.tsx
+++ src/hooks/Countereducer.tsx
@@ -1,4 +1,11 @@
 
 export const Countereducer = (state,action) =>{
+    switch (action.payload) {
+        case value:
 
+            break;
+    
+        default:
+            return state;
+    }
 }
