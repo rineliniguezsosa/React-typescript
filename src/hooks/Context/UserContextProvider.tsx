c115d17444e8e3fd6b2684218a720819584f38e6
rinel <rineliniguezsosa@gmail.com>
2023-06-07 17:43:42 -0600

    fix: corrigiendo el nombre del type UserProviderprops  y las coincidencias por UserContextProviderprops

--- src/hooks/Context/UserContextProvider.tsx
+++ src/hooks/Context/UserContextProvider.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { UserContext } from './UserContext'
 import { useState } from 'react';
 
-type UserProviderprops = {
+type UserContextProviderprops = {
     children:React.ReactNode
 }
 type Authuser = {
     name:string,
     email:string
 }
-export const UserContextProvider = ({children}:UserProviderprops) => {
+export const UserContextProvider = ({children}:UserContextProviderprops) => {
     const [user, setUser] = useState<Authuser | null>(null)
   return (
     <UserContext.Provider>
