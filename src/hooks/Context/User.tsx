ba8492df7cdc74e0341e32ae5f071712a611d917
rinel <rineliniguezsosa@gmail.com>
2023-06-07 16:55:10 -0600

    feat(User): pegando el contenido de User.tsx

---
+++ src/hooks/Context/User.tsx
@@ -0,0 +1,16 @@
+
+export const User = () => {
+    const handlelogin = () =>{
+        
+    }
+    const handlelogout = () =>{
+        
+    }
+  return (
+    <div>
+        <button onClick={handlelogin}>Login</button>
+        <button onClick={handlelogout}>Logout</button>
+        <span>El usuario es</span>
+    </div>
+  )
+}
