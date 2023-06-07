209e9b6381139ebb73c131d7e7ce1cfbb742f19f
rinel <rineliniguezsosa@gmail.com>
2023-06-07 17:08:41 -0600

    feat(UserProvider): creando el type UserProviderprops
    tipando la envoltura children

--- src/hooks/Context/UserProvider.tsx
+++ src/hooks/Context/UserProvider.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { UserContext } from './UserContext'
 
+type UserProviderprops = {
+    children:React.ReactNode
+}
 export const UserProvider = () => {
   return (
     <div>UserProvider</div>
