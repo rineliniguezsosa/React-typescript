e6780086ebecfe20c050b3b0e006f55693ea6229
rinel <rineliniguezsosa@gmail.com>
2023-06-06 18:34:42 -0600

    feat(Counter): inicializando el state de count en cero y definiendo el hook usereducer

--- src/hooks/Counter.tsx
+++ src/hooks/Counter.tsx
@@ -1,6 +1,10 @@
 import { useReducer } from "react"
 
+const initialState = {
+    count:0
+}
 export const Counter = () => {
+  const [state, dispatch] = useReducer(countreducer,initialState)
   return (
     <div>Counter</div>
   )
