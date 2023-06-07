208ed92573439f37de92e0b2e39e8ec9e995da69
rinel <rineliniguezsosa@gmail.com>
2023-06-07 14:44:43 -0600

    feat(ThemeProvider): creando el type ThemeProviderprops
    tipando el children como ReactNode

--- src/hooks/Context/ThemeProvider.tsx
+++ src/hooks/Context/ThemeProvider.tsx
@@ -1,6 +1,9 @@
 import { ThemeContext } from "./ThemeContext"
 import React from 'react'
 
+type ThemeProviderprops = {
+    children:React.ReactNode
+}
 export const ThemeProvider = () => {
   return (
     <div>ThemeProvider</div>
