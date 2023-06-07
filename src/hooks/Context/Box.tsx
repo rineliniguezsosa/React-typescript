f1ee890fc92d9b6d544b0380c3d25d5988228c4c
rinel <rineliniguezsosa@gmail.com>
2023-06-07 16:45:22 -0600

    feat(Box): agregando la propiedad style y accediendo a los valores de la variable theme

--- src/hooks/Context/Box.tsx
+++ src/hooks/Context/Box.tsx
@@ -5,6 +5,6 @@ import { ThemeContext } from "./ThemeContext"
 export const Box = () => {
     const theme = useContext(ThemeContext)
   return (
-    <div>Box</div>
+    <div style={{backgroundColor:theme.primary.main,color:theme.primary.text}}>Theme context</div>
   )
 }
