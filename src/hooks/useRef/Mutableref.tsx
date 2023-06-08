450c63100b9ef4829af81981088d33949bd08bbb
rinel <rineliniguezsosa@gmail.com>
2023-06-07 21:30:16 -0600

    feat(Mutableref): creando la variable intervalRef
    establecemos por referencia el hook useRef a la variable intervalRef y lo inicializaremos en null

--- src/hooks/useRef/Mutableref.tsx
+++ src/hooks/useRef/Mutableref.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 
 export const Mutableref = () => {
     const [timer, setTimer] = useState(0)
+    const intervalRef = useRef(null)
   return (
     <div>Mutableref</div>
   )
