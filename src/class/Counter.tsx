bdfc98a1a9ead7028843105b078d59f00629a1b4
rinel <rineliniguezsosa@gmail.com>
2023-06-08 00:10:34 -0600

    feat(Counter): creando la función increment y actualizando el state count sumandole el valor de uno al count

--- src/class/Counter.tsx
+++ src/class/Counter.tsx
@@ -4,11 +4,16 @@ export class Counter extends Component {
     state = {
         count:0
     }
+
+    increment =() =>{
+        this.setState({count:this.state.count+1})
+    }
   render() {
     return (
       <div>
         Counter
         <button onClick={this.increment}>Incrementar</button>
+        {this.state.count}
       </div>
     )
   }
