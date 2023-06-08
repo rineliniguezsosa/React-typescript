aba2086aa9cdcf2290097b37a870d4c493246876
rinel <rineliniguezsosa@gmail.com>
2023-06-07 21:05:57 -0600

    feat(Domref): establecemos la funcion useEffect
    useEffect se ejecutara una sola vez sin dependencias y simplemente enfocaremos el elemento input

--- src/hooks/useRef/Domref.tsx
+++ src/hooks/useRef/Domref.tsx
@@ -3,6 +3,10 @@ import { useRef,useEffect } from "react"
 
 export const Domref = () => {
     const domref = useRef(null)
+
+    useEffect(() => {
+      domref.current.focus()
+    }, [])
   return (
     <div>
         <input type="text" ref={domref} />
