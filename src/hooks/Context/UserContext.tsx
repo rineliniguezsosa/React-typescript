9e2fb380bf2ffe082e4c77e500e698f127fad55b
rinel <rineliniguezsosa@gmail.com>
2023-06-07 19:32:52 -0600

    feat(UserContext): importando Authuser y pegando el type UserContexttype

--- src/hooks/Context/UserContext.tsx
+++ src/hooks/Context/UserContext.tsx
@@ -1,3 +1,9 @@
 import { createContext } from 'react'
+import { Authuser } from './UserContextProvider';
+
+type UserContexttype = {
+    user:Authuser | null,
+    setUser:React.Dispatch<React.SetStateAction<Authuser | null>> //tipando la accion
+}
 
 export const UserContext = createContext(null)
